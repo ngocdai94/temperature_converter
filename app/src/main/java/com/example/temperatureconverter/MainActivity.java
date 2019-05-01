f55ecd75084abd132426ab7a72a03b5c5449cf40
Dai Nguyen <ngocdai94@gmail.com>
2019-05-01 12:52:40 -0700

    Add Resume and Pause for the App

--- app/src/main/java/com/example/temperatureconverter/MainActivity.java
+++ app/src/main/java/com/example/temperatureconverter/MainActivity.java
@@ -1,24 +1,37 @@
 package com.example.temperatureconverter;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
+import android.widget.TextView.OnEditorActionListener;
 import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements OnEditorActionListener {
+
+    // define variables for the widgets
+    private EditText fTemperature;
+    private TextView cTemperature;
+
+    // define the SharedPreferences object
+    private SharedPreferences savedValues;
+
+    // define instance variables that should be saved
+    private String fDegree_String = "";
+    private float c_Degree = 0;
+    private float f_Degree = 0;
 
     /**
      * Decimal Format Class
      * Link 1: https://developer.android.com/reference/java/text/DecimalFormat
      * Link 2: https://stackoverflow.com/questions/8065114/how-to-print-a-double-with-two-decimals-in-android
      */
-    DecimalFormat precision = new DecimalFormat("0.000");
+    private DecimalFormat precision = new DecimalFormat("0.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,27 +42,71 @@ public class MainActivity extends AppCompatActivity {
          * Event Input Handler - Android Studio
          * https://developer.android.com/training/keyboard-input/style#java
          */
-        final EditText fTemperature = (EditText) findViewById(R.id.fDegree);
-        final TextView cTemperature = findViewById(R.id.cDegree);
-        fTemperature.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                boolean handled = false;
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    convertFtoC();
-                    handled = true;
-                }
-                return handled;
-            }
+        // get reference to widget
+        fTemperature = (EditText) findViewById(R.id.fDegree);
+        cTemperature = (TextView) findViewById(R.id.cDegree);
 
-            private void convertFtoC() {
-                // F to C Calculations
-                float get_F_Temperature = Float.valueOf(fTemperature.getText().toString());
-                float cDegree = (get_F_Temperature - 32) * 5 / 9;
+        // set the listeners
+        fTemperature.setOnEditorActionListener(this);
 
-                // Display the calculation temperature
-                cTemperature.setText(precision.format(cDegree));
-            }
-        });
+        // get SharedPreferences object
+        savedValues = getSharedPreferences("SavedValues", MODE_PRIVATE);
+    }
+
+    /**
+     * This method will listen and respond to event after input
+     *
+     * @param v
+     * @param actionId
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        boolean handled = false;
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+            convertFtoC();
+            handled = true;
+        }
+        return handled;
+    }
+
+    @Override
+    public void onPause() {
+        // save the instance variables
+        SharedPreferences.Editor editor = savedValues.edit();
+        editor.putString("fDegree_String", fDegree_String);
+        editor.commit();
+
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // get the instance variables
+        fDegree_String = savedValues.getString("fDegree_String", "");
+
+        // set the F temperature on its widget
+        fTemperature.setText(fDegree_String);
+
+        // calculate F to C temperature
+        convertFtoC();
+    }
+
+    public void convertFtoC() {
+        // F to C Calculations
+        fDegree_String = fTemperature.getText().toString();
+
+        if (fDegree_String.equals("")) {
+            f_Degree = 0;
+        } else {
+            f_Degree = Float.parseFloat(fDegree_String);
+        }
+        c_Degree = (f_Degree - 32) * 5 / 9;
+
+        // Display the calculation temperature
+        cTemperature.setText(precision.format(c_Degree));
     }
 }
